8f90760e4e7d6fade0a3badb364c4812131e8fe5
TonyKodit <onwulinonso@gmail.com>
2024-04-14 07:45:50 +0100

    EntityFramework Core Database Models

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,5 +1,4 @@
 
-using MagicVilla_VillaAPI.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +11,6 @@ builder.Services.AddControllers(option => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ILogging, LoggingV2>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
